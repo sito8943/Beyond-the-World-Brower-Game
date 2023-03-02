56c84d93cc70f9b1da079ac310ab7b2bc15eff1b
Sito8943 <sito8943@gmail.com>
2023-03-01 20:13:00 -0800

    bottom bar

--- src/components/Board/BottomBar.jsx
+++ src/components/Board/BottomBar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TurnButton from "../IconButtons/TurnButton";
+import Portrait from "../Portrait/Portrait";
 
 // styles
 import "./styles.css";
@@ -9,7 +10,11 @@ export default function BottomBar() {
     <div className="w-full relative flex items-center justify-between">
       <div className="absolute bottom-bar w-full bg-dark-primary"></div>
       <div className="absolute w-full">
-        <div className="menu flex items-center justify-start"></div>
+        <div className="menu flex items-center justify-start">
+          {[0, 1, 2, 3].map((item) => (
+            <Portrait key={item} />
+          ))}
+        </div>
         <div className="resources flex items-center justify-end px-3">
           <TurnButton className="end-turn text-white rounded-circle bg-light-primary" />
         </div>
